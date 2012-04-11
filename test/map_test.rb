b97615cb504e375a9abb684a0d36e4fb3fa58011
Jia Brown <wujia123@yahoo.com>
2012-04-11 18:55:55 -0400

    test to move objects

--- test/map_test.rb
+++ test/map_test.rb
@@ -2,11 +2,20 @@ require_relative "helper"
 require_relative "../lib/blind/map"
 
 describe Blind::Map do
-  it "must be able to store elements at a position" do
+  it "must be able to store objects at a position" do
     map = Blind::Map.new
     map.place("player", 10, 25)
 
     pos = map.locate("player")
     [pos.x, pos.y].must_equal([10,25])
   end
+  
+  it "must be able to move objects" do
+    map = Blind::Map.new
+    map.place("player", 10, 25)
+    map.move("player", 5, 50)
+    pos = map.locate("player")
+    [pos.x, pos.y].must_equal([15,75])    
+  end
+  
 end
