01c4d245f81d2d0529e5c6521ca1666c4cb0f5e7
Gregory Brown <gregory.t.brown@gmail.com>
2012-04-11 19:00:42 -0400

    Pass movement test

--- lib/blind/map.rb
+++ lib/blind/map.rb
@@ -1,12 +1,22 @@
-require "ray"
-
 module Blind
   class Map
-    def place(*a)
+    Location = Struct.new(:x, :y)
+
+    def initialize
+      @objects = {}
+    end
+
+    def place(name, x, y)
+      @objects[name] = [x,y]
     end
 
     def locate(name)
-      Struct.new(:x,:y).new(10,25)
+      Location.new(*@objects[name])
+    end
+
+    def move(name, x,y)
+      xo, yo = @objects[name]
+      @objects[name] = [xo + x, yo + y]
     end
   end
 end
